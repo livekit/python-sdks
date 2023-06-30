6d61c31bf258343e9256f94e1749d4d0f13f2668
Théo Monnom <theo.monnom@outlook.com>
2023-06-30 22:49:38 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="livekit",
-    version="0.0.1",
+    version="0.1.0",
     description="A sample Python project",
     long_description=long_description,
     long_description_content_type="text/markdown",
