f6a24610d123be4cbb29fbd6a9c766077c3dbf83
Théo Monnom <theo.monnom@outlook.com>
2023-09-08 14:22:07 -0700

    max stream capacity & fix leaks

---
+++ livekit/_utils.py
@@ -0,0 +1,23 @@
+import asyncio
+from collections import deque
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+
+class RingQueue(Generic[T]):
+    def __init__(self, capacity: int = 0) -> None:
+        self._capacity = capacity
+        self._queue: deque[T] = deque()
+        self._event = asyncio.Event()
+
+    def put(self, item: T) -> None:
+        if self._capacity > 0 and len(self._queue) == self._capacity:
+            self._queue.pop()
+        self._queue.append(item)
+        self._event.set()
+
+    async def get(self) -> T:
+        while not self._queue:
+            await self._event.wait()
+        return self._queue.popleft()
