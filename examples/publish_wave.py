dcf36d33f23d48059c821050f9ad233140f1158d
David Zhao <dz@livekit.io>
2023-09-20 18:54:12 -0700

    publish multiple tracks

--- examples/publish_wave.py
+++ examples/publish_wave.py
@@ -13,7 +13,7 @@ SAMPLE_RATE = 48000
 NUM_CHANNELS = 1
 
 
-async def publish_frames(source: livekit.AudioSource):
+async def publish_sine(source: livekit.AudioSource):
     frequency = 440
     amplitude = 32767  # for 16-bit audio
     samples_per_channel = 480  # 10ms at 48kHz
@@ -37,24 +37,75 @@ async def publish_frames(source: livekit.AudioSource):
         total_samples += samples_per_channel
 
 
+async def publish_pulse(source: livekit.AudioSource):
+    frequency = 440
+    modulation_frequency = 1.0  # 1Hz for beep once every second
+    beep_duration = 0.5  # Beep for half a second, then silence for half a second
+    amplitude = 32767  # for 16-bit audio
+    samples_per_channel = 480  # 10ms at 48kHz
+    total_samples = 0
+
+    audio_frame = livekit.AudioFrame.create(
+        SAMPLE_RATE, NUM_CHANNELS, samples_per_channel)
+
+    audio_data = np.ctypeslib.as_array(audio_frame.data)
+
+    while True:
+        time = (total_samples + np.arange(samples_per_channel)) / SAMPLE_RATE
+
+        sine_wave = amplitude * np.sin(2 * np.pi * frequency * time)
+
+        # Square wave for clear beep effect
+        square_wave = (
+            np.sign(np.sin(2 * np.pi * modulation_frequency * time)) + 1) / 2
+        # Zero out the sine wave where the square wave is zero (during the "off" portion of the beep)
+        beeping_sine = sine_wave * square_wave
+
+        # Limit beep duration
+        beeping_sine[time % (1/modulation_frequency) > beep_duration] = 0
+
+        np.copyto(audio_data, beeping_sine.astype(np.int16))
+
+        await source.capture_frame(audio_frame)
+
+        total_samples += samples_per_channel
+
+
 async def main(room: livekit.Room) -> None:
     logging.info("connecting to %s", URL)
     try:
-        await room.connect(URL, TOKEN)
+        e2ee_options = livekit.E2EEOptions()
+        e2ee_options.key_provider_options.shared_key = b'livekitrocks'
+
+        await room.connect(URL, TOKEN, options=livekit.RoomOptions(
+            e2ee=e2ee_options
+        ))
         logging.info("connected to room %s", room.name)
     except livekit.ConnectError as e:
         logging.error("failed to connect to the room: %s", e)
         return
 
+    @room.on('e2ee_state_changed')
+    def e2ee_changed(participant, state):
+        logging.info("decryption changed %s: %s", participant.identity, state)
+
     # publish a track
     source = livekit.AudioSource(SAMPLE_RATE, NUM_CHANNELS)
     track = livekit.LocalAudioTrack.create_audio_track("sinewave", source)
-    options = livekit.TrackPublishOptions()
-    options.source = livekit.TrackSource.SOURCE_MICROPHONE
+    options = livekit.TrackPublishOptions(
+        source=livekit.TrackSource.SOURCE_MICROPHONE,
+    )
     publication = await room.local_participant.publish_track(track, options)
     logging.info("published track %s", publication.sid)
+    asyncio.ensure_future(publish_sine(source))
 
-    asyncio.ensure_future(publish_frames(source))
+    await asyncio.sleep(1)
+
+    source2 = livekit.AudioSource(SAMPLE_RATE, NUM_CHANNELS)
+    track2 = livekit.LocalAudioTrack.create_audio_track("sinewave2", source2)
+    publication = await room.local_participant.publish_track(track2, options)
+    logging.info("published track 2 %s", publication.sid)
+    asyncio.ensure_future(publish_sine(source2))
 
 
 if __name__ == "__main__":
