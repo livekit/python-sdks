fa5aba24c2bbf1d9ee1013d28a76078aed88ed86
Théo Monnom <theo.monnom@outlook.com>
2023-11-13 17:59:25 -0800

    loop is deprecated

--- livekit-api/livekit/api/api.py
+++ livekit-api/livekit/api/api.py
@@ -10,10 +10,9 @@ class LiveKitAPI:
         api_key: str = os.getenv("LIVEKIT_API_KEY", ""),
         api_secret: str = os.getenv("LIVEKIT_API_SECRET", ""),
         *,
-        loop: Optional[asyncio.AbstractEventLoop] = asyncio.get_event_loop(),
         timeout: float = 60,  # 1 minutes by default
     ):
-        self._session = aiohttp.ClientSession(timeout=timeout, loop=loop)
+        self._session = aiohttp.ClientSession(timeout=timeout)
         self._room = RoomService(url, api_key, api_secret, self._session)
         self._ingress = IngressService(url, api_key, api_secret, self._session)
         self._egress = EgressService(url, api_key, api_secret, self._session)
