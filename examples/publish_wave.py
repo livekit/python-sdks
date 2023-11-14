1a57aabf43bf22619ebf1361f022246a42f3105c
Théo Monnom <theo.monnom@outlook.com>
2023-11-13 23:47:45 -0800

    undo

--- examples/publish_wave.py
+++ examples/publish_wave.py
@@ -24,7 +24,7 @@ async def main(room: rtc.Room) -> None:
         .with_grants(
             api.VideoGrants(
                 room_join=True,
-                room="test",
+                room="my-room",
             )
         )
         .to_jwt()
