d2ca4f7c88d27d794f5c98ec48d58921a27727d6
Denys Smirnov <dennwc@pm.me>
2024-06-21 15:12:38 +0300

    Update to Python 3.10.

--- livekit-api/livekit/api/_service.py
+++ livekit-api/livekit/api/_service.py
@@ -16,7 +16,7 @@ class Service(ABC):
         self.api_secret = api_secret
 
     def _auth_header(
-        self, grants: VideoGrants, sip: SIPGrants = None
+        self, grants: VideoGrants, sip: SIPGrants | None = None
     ) -> Dict[str, str]:
         tok = AccessToken(self.api_key, self.api_secret).with_grants(grants)
         if sip is not None:
