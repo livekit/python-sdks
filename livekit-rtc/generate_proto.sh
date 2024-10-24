4c91c94a05468f1c76175aa16fbe5a262904a8b6
Ben Cherry <ben@livekit.io>
2024-10-24 15:17:10 -0700

    rpc proto

--- livekit-rtc/generate_proto.sh
+++ livekit-rtc/generate_proto.sh
@@ -33,10 +33,11 @@ protoc \
     $FFI_PROTOCOL/track.proto \
     $FFI_PROTOCOL/video_frame.proto \
     $FFI_PROTOCOL/e2ee.proto \
-    $FFI_PROTOCOL/stats.proto
+    $FFI_PROTOCOL/stats.proto \
+    $FFI_PROTOCOL/rpc.proto
 
 touch -a "$FFI_OUT_PYTHON/__init__.py"
 
 for f in "$FFI_OUT_PYTHON"/*.py "$FFI_OUT_PYTHON"/*.pyi; do
-    perl -i -pe 's|^(import (audio_frame_pb2\|ffi_pb2\|handle_pb2\|participant_pb2\|room_pb2\|track_pb2\|video_frame_pb2\|e2ee_pb2\|stats_pb2))|from . $1|g' "$f"
+    perl -i -pe 's|^(import (audio_frame_pb2\|ffi_pb2\|handle_pb2\|participant_pb2\|room_pb2\|track_pb2\|video_frame_pb2\|e2ee_pb2\|stats_pb2|rpc_pb2))|from . $1|g' "$f"
 done
