b13435b16e50285ebe5eeaf4872268aa67612094
Ben Cherry <ben@livekit.io>
2024-10-28 14:54:04 -0700

    ex

--- livekit-rtc/livekit/rtc/__init__.py
+++ livekit-rtc/livekit/rtc/__init__.py
@@ -72,7 +72,7 @@ from .video_source import VideoSource
 from .video_stream import VideoFrameEvent, VideoStream
 from .audio_resampler import AudioResampler, AudioResamplerQuality
 from .utils import combine_audio_frames
-from .rpc import RpcError
+from .rpc import RpcError, RpcInvocationData
 
 __all__ = [
     "ConnectionQuality",
