a062bef3f8790fb7858a005aeeda0839444cda11
David Zhao <dz@livekit.io>
2024-11-05 18:30:05 -0600

    fix

--- examples/api.py
+++ examples/api.py
@@ -15,6 +15,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
+    asyncio.run(main())
